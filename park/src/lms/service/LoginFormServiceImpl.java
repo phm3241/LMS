bc73ca78793e202ad77a0304f3aae809780ca165
phm3241 <parkhm3241@gmail.com>
2020-07-22 18:42:45 +0900

    lms. 로그인

--- park/src/lms/service/LoginFormServiceImpl.java
+++ park/src/lms/service/LoginFormServiceImpl.java
@@ -3,8 +3,7 @@ package lms.service;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// 로그인 처리 후 ㅡ> 어떤 타입으로 로그인했는지에 따라 ㅡ> 마이페이
-public class loginServiceImpl implements Service {
+public class LoginFormServiceImpl implements Service {
 
 	// Service 클래스 이용에 대한 예제 클래스입니다.
 	
@@ -12,9 +11,10 @@ public class loginServiceImpl implements Service {
 	@Override
 	// 
 	public String getViewPage(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
 		
 		// Front Controller에 view 페이지를 요청합니다.
-		return "/WEB-INF/login.jsp";
+		return "/WEB-INF/loginForm.jsp";
 	}
 
 
