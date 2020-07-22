ac34824003ee2001541533f896c7b2124b3e9c4f
s1nwoo <tizm7777@naver.com>
2020-07-22 15:10:32 +0900

    dto, dao 수정(search)

--- jung/LMS_Frame/src/lms/dao/TeacherDao.java
+++ jung/LMS_Frame/src/lms/dao/TeacherDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import lms.model.Student;
 import lms.model.Teacher;
 import lms.model.TempletModel;
 
@@ -21,6 +22,44 @@ public class TeacherDao {
 		return dao;
 	}
 
+	// 교수 정보 입력
+	public int insertTeacher(Connection conn, Teacher teacher) throws SQLException {
+
+		int result = 0;
+
+		// sql 쿼리문을 처리할 statement를 설정합니다.
+		PreparedStatement pstmt = null;
+
+		// 처리하고 싶은 sql 쿼리문과 조건을 설정합니다.
+		// 이런 테이블이 있다는 가정하에 작성한 spl문
+		String sql = "INSERT INTO project.teacher (tIdx, pw, name, tel, email, major, job) "
+				+ "	VALUES(?, ?, ?, ?, ?, ?, ?)";
+
+		try {
+
+			// sql문을 실행합니다.
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, teacher.gettIdx());
+			pstmt.setString(2, teacher.getPw());
+			pstmt.setString(3, teacher.getName());
+			pstmt.setString(4, teacher.getTel());
+			pstmt.setString(5, teacher.getEmail());
+			pstmt.setString(6, teacher.getMajor());
+			pstmt.setString(7, teacher.getJob());
+
+			result = pstmt.executeUpdate();
+			
+
+		} finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
+
+		return result;
+
+	}
+	
 	// 교수 내정보 수정
 	public int editTeacher(Connection conn, Teacher teacher) throws SQLException {
 
