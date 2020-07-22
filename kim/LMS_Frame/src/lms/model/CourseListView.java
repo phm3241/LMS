ac08e94022930c23d8ab028b5ef3945175b8f9c6
KimSeungYeon <rlamd0412@gmail.com>
2020-07-22 11:41:28 +0900

    CourseDao / CourseListView 모델링
    List에 강의정보 저장하고 List 형태로 View 하는 거 까지 만듦!

---
+++ kim/LMS_Frame/src/lms/model/CourseListView.java
@@ -0,0 +1,32 @@
+package lms.model;
+
+import java.util.List;
+
+//자신이 사용할 Dao/Model/Service resurve 파일 번호를 항상 공유해주세용!
+public class CourseListView {
+	
+	private List<Course> courseList;
+
+	
+	public CourseListView(List<Course> courseList) {
+		this.courseList = courseList;
+	}
+
+	
+	public List<Course> getCourseList() {
+		return courseList;
+	}
+
+	
+	public void setCourseList(List<Course> courseList) {
+		this.courseList = courseList;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "CourseListView [courseList=" + courseList + "]";
+	}
+	
+	
+}
