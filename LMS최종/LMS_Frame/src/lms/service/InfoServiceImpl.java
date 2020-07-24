30ba9427a458d4f3a89f21361c4c62a1700cb513
Ga Ram Song <21400389@handong.edu>
2020-07-24 16:02:22 +0900

    Info Service 세트 완성

--- LMS최종/LMS_Frame/src/lms/service/InfoServiceImpl.java
+++ LMS최종/LMS_Frame/src/lms/service/InfoServiceImpl.java
@@ -4,23 +4,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class EditFormServiceImpl implements Service {
+public class InfoServiceImpl implements Service {
 
 	// 자신이 사용할 Dao/Model/Service resurve 파일 번호를 항상 공유해주세용!
 	
-	
 	@Override
 	public String getViewPage(HttpServletRequest request, HttpServletResponse response) {
+		// session에서 로그인 타입과 객체 불러오기
 		HttpSession session = request.getSession(false);
 		String type = (String) session.getAttribute("loginType");
 		
+		// 변수 초기화
 		String path = null;
 		
-		if(type.equals("sLogin")) {
+
+		if(type.equals("sLogin")) {			
 			path = "/WEB-INF/views/student/sInfo.jsp";
-		} else if(type.equals("tLogin")) {
+		} else {
 			path = "/WEB-INF/views/teacher/tInfo.jsp";
 		}
+		
 		return path;
 	}
 
