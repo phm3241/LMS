4cce9f57f4f7cb3a0bec8e83cb916bb7ebfadbbd
zannew <63032810+zannew@users.noreply.github.com>
2020-07-24 12:03:33 +0900

    r

--- jang/LMS_Frame/src/lms/dao/CourseDao.java
+++ jang/LMS_Frame/src/lms/dao/CourseDao.java
@@ -32,13 +32,15 @@ public class CourseDao {
 		int result = 0;
 
 		PreparedStatement pstmt = null;
+		
+		
 		String sql = "INSERT INTO project.class (`name`,`teacher`,`content`, `day`,`startTime`,`endTime`,`totalPer`,`applyPer`,`tIdx`)\r\n"
 				+ "VALUES (?,?,?,?,?,?,?,?,?)";
 
 		// 마지막값 tIdx는 교수번호를 Model에서 가져와야..
 
 		try {
-
+			
 			pstmt = conn.prepareStatement(sql);
 //			pstmt.setInt(1, course.getcIdx()); 강의번호는 자동생성
 			pstmt.setString(1, course.getName());
@@ -49,6 +51,8 @@ public class CourseDao {
 			pstmt.setInt(7, course.getTotalPer());
 			pstmt.setInt(8, course.getApplyPer());
 			pstmt.setInt(10, course.gettIdx()); 
+			
+			result = pstmt.executeUpdate();
 
 		} finally {
 			if (pstmt != null) {
@@ -59,6 +63,9 @@ public class CourseDao {
 		return result;
 	}
 
+	
+
+	
 	// 개설강의 이름으로 조회 : select (복수일 경우를 대비하여 list)
 	public List<Course> selectCourseByNameList (Connection conn, String name) throws SQLException {
 		
@@ -67,7 +74,7 @@ public class CourseDao {
 		
 		List<Course> selectCourseByNameList = new ArrayList<Course>();
 		
-		String sql = "SELECT * FROM project.course where name=?";
+		String sql = "SELECT * FROM project.course where name=?;";
 		
 		try {
 			
@@ -153,13 +160,12 @@ public class CourseDao {
 		return result;
 
 	}
+	
 
 	// 수강신청한 강의 리스트 조회 : myCourse 조회 = select ?
-	//0724 윤원수정
-	public List<MyCourse> selectMyCourse(Connection conn, Course course, Student student) throws SQLException {
+	public int selectMyCourse(Connection conn, Course course, Student student) throws SQLException {
 
-		List<MyCourse> resultList = null;
-		MyCourse myCourse=null;
+		int result = 0;
 
 		PreparedStatement pstmt = null;
 		ResultSet rs;
@@ -174,10 +180,7 @@ public class CourseDao {
 			rs = pstmt.executeQuery();
 
 			if (rs.next()) {
-				myCourse = new MyCourse(student.getsIdx(), rs.getInt("cIdx"));
-				
-				resultList.add(myCourse);
-				
+				result = rs.getInt(1);
 			}
 
 		} finally {
@@ -186,8 +189,82 @@ public class CourseDao {
 			}
 		}
 
-		return resultList;
+		return result;
 
+	}
+	
+	
+	// MyCourse 에 학생번호와 강의번호 Insert
+	public int insertMyCourse (Connection conn, MyCourse mycourse) throws SQLException{
+		
+		int result = 0;
+		
+		PreparedStatement pstmt = null;
+		
+		String sql = "INSERT INTO `project`.`myCourse` (`sIdx`, `cIdx`)VALUES (?,?);";
+		
+		try {
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mycourse.getsIdx());
+			pstmt.setInt(2, mycourse.getcIdx());
+			
+			result = pstmt.executeUpdate();
+			
+		}finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
+		
+		return result;
+		
+	}
+	
+	
+	// MyCourse 에서 학생 아이디로 조회 후 LEFT JOIN한 cIdx 데이터 리스트 저장
+	public List<Course> selectMyCourseBysIdx (Connection conn, int sIdx) throws SQLException{
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<Course> selectMyCourseBysIdx = new ArrayList<Course>();
+		
+		String sql = "SELECT myCourse.*, course.* FROM myCourse" + 
+				"LEFT JOIN course on myCourse.cIdx = course.cIdx where myCourse.sIdx=?;";
+		
+		try {
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				Course course = new Course();
+				course.setcIdx(rs.getInt("cIdx"));
+				course.setName(rs.getString("name"));
+				course.setTeacher(rs.getString("teacher"));
+				course.setContent(rs.getString("content"));
+				course.setDay(rs.getString("day"));
+				course.setStartTime(rs.getInt("startTime"));
+				course.setTotalPer(rs.getInt("totalPer"));
+				course.setApplyPer(rs.getInt("applyPer"));
+				course.settIdx(rs.getInt("tIdx")); 
+				
+				selectMyCourseBysIdx.add(course);
+			}
+			
+		} finally {
+			if(rs != null) {
+				rs.close();
+			}
+			
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			
+		}
+		return selectMyCourseBysIdx;
 	}
 
 	// 수강신청한 강의 취소 : myCourse 조회 => delete ?
